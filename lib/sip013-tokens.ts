faedb1dec47732d878ba9cb248283420d7422a20
diablo <solsticeveil46@gmail.com>
2026-02-12 10:40:43 +0100

    feat(sip013): Add SIP-013 feature 6

--- lib/sip013-tokens.ts
+++ lib/sip013-tokens.ts
@@ -13,3 +13,5 @@ export const SIP013_FEATURE_3 = 'feature_3';
 export const SIP013_FEATURE_4 = 'feature_4';
 
 export const SIP013_FEATURE_5 = 'feature_5';
+
+export const SIP013_FEATURE_6 = 'feature_6';
