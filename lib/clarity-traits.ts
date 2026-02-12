0712b1da1d5616e8adcde68d5257495030ba3557
diablo <solsticeveil46@gmail.com>
2026-02-12 10:43:27 +0100

    feat(clarity-traits): Add SIP-013 semi-fungible trait interface

--- lib/clarity-traits.ts
+++ lib/clarity-traits.ts
@@ -15,3 +15,8 @@ export interface SIP010Trait extends ClarityTrait {
   name: 'sip010-ft-trait';
   methods: ['get-name', 'get-symbol', 'get-decimals', 'get-balance', 'get-total-supply', 'transfer'];
 }
+
+export interface SIP013Trait extends ClarityTrait {
+  name: 'sip013-semi-fungible-token-trait';
+  methods: ['get-balance', 'get-overall-balance', 'get-total-supply', 'transfer'];
+}
